1a14727c439efb6e1b4f4b922379359c7defdd9d
JaviC5626 <167147805+JaviC5626@users.noreply.github.com>
2024-09-16 19:59:40 -0500

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>train up</Text>
+      <Text>Train Up</Text>
       <StatusBar style="auto" />
     </View>
   );
